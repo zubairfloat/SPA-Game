c701625d01a3fb3d13c1a6d50b118fbdf9d1471d
zubair <zubairfloat@gmail.com>
2022-03-30 15:33:50 +0500

    remove errors

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Header from "../components/Header";
-import Footer from "../components/Footer";
+// import Footer from "../components/Footer";
 import MobHeader from "../components/MobHeader";
 
 const Layout = ({ children }) => {
